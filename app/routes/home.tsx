2f27b9d84cacc254094b1594eb1465ad9222d623
Manuel Fedyna <manuelfedyna@gmail.com>
2025-05-21 14:19:14 +0000

    feat: schemaDone

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,54 @@
 import { useReducer, useEffect } from "react";
 import * as V from "valibot";
 
+const StatSchema = V.union([
+  V.object({}),
+  V.record(V.pipe(V.string(), V.minLength(4)), V.number()),
+]);
+
+const MapsSchema = V.object({
+  11: V.boolean(),
+  12: V.boolean(),
+  21: V.boolean(),
+  22: V.boolean(),
+  30: V.boolean(),
+  33: V.boolean(),
+});
+
+const IntoSchema = V.array(V.pipe(V.string(), V.minLength(5)));
+
+const ImageSchema = V.object({
+  full: V.pipe(V.string(), V.minLength(8)),
+  group: V.pipe(V.string(), V.minLength(3)),
+  h: V.number(),
+  sprite: V.pipe(V.string(), V.minLength(7)),
+  w: V.number(),
+  x: V.number(),
+  y: V.number(),
+});
+
+const GoldSchema = V.object({
+  base: V.number(),
+  purchasable: V.boolean(),
+  sell: V.number(),
+  total: V.number(),
+});
+
+const ItemSchema = V.object({
+  colloq: V.string(),
+  description: V.string(),
+  gold: GoldSchema,
+  image: ImageSchema,
+  into: IntoSchema,
+  maps: MapsSchema,
+  name: V.pipe(V.string(), V.minLength(3)),
+  plantext: V.pipe(V.string(), V.minLength(4)),
+  stat: StatSchema,
+  tags: V.array(V.pipe(V.string(), V.minLength(4))),
+});
+
+type Item = V.InferOutput<typeof ItemSchema>;
+
 //@ts-ignore
 function itemsReducer(state, action) {
   switch (action.type) {
@@ -26,7 +74,8 @@ export default function () {
       ).then((response) => {
         return response.json();
       });
-      dispatch({ type: "GET_ITEMS", payload: allLolItems });
+      const dataLolitems = V.parse(ItemSchema, allLolItems.data);
+      dispatch({ type: "GET_ITEMS", payload: dataLolitems });
     }
     run();
   }, []);
